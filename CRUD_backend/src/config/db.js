07d181da5f51ff01394f1c7697f79c9e00571110
CM-Kali <cmkali2126@gmail.com>
2026-02-01 08:28:44 +0500

    Data base connected

---
+++ CRUD_backend/src/config/db.js
@@ -0,0 +1,16 @@
+const mongoose=require("mongoose");
+
+const connectDB=async ()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Mongo db connected");
+    }
+    catch(error){
+        console.log("Mongo db connection failed");
+        process.exit(1);
+
+    };
+    
+}
+
+module.exports=connectDB;
