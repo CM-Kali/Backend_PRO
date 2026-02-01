4ebdbbc1ee3d8b423f94724f2888e10ca8bbd868
CM-Kali <cmkali2126@gmail.com>
2026-02-01 08:45:16 +0500

    Server created also usermodel add app module created

---
+++ CRUD_backend/server.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+const app=require("./src/app");
+const connectDB=require("./src/config/db");
+
+connectDB();
+
+app.listen(5000,()=>{
+    console.log("Server is running at http://localhost:5000");
+})
