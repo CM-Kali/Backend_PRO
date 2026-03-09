899e3cf1f0bf0fd9d85f235ec077d6679438c0cf
Muhammad Adeel Abid <cmkali2126@gmail.com>
2026-03-09 13:39:19 +0500

    User model and controller cretaed

--- CRUD_FullStack/server.js
+++ CRUD_FullStack/server.js
@@ -19,8 +19,8 @@ app.use(express.json());
 //Databasae Connection
 
 
-//const PORT=process.env.PORT || 5000;
+const PORT=process.env.PORT || 5000;
 
 app.listen(5000,()=>{
-       console.log("server is running on ${PORT}");
+    console.log('server is running on http://localhost:5000/');
 })
