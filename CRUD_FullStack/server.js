3fa1d6c239ed0e32c762301c919b97653c6a706d
Muhammad Adeel Abid <cmkali2126@gmail.com>
2026-03-09 13:21:26 +0500

    Full stack CRUD application with proper syetem design started

---
+++ CRUD_FullStack/server.js
@@ -0,0 +1,26 @@
+const express=require("express");
+const mongoose=require("mongoose");
+const cors=require("cors");
+require("dotenv").config();
+
+const userRoutes=require("./routes/userRoutes");
+
+const app=express();    
+
+//Middle Ware 
+
+app.use(cors());
+app.use(express.json());
+
+
+//routes
+
+
+//Databasae Connection
+
+
+//const PORT=process.env.PORT || 5000;
+
+app.listen(5000,()=>{
+       console.log("server is running on ${PORT}");
+})
